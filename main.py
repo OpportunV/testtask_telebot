577203e43f48f542fc83a0d71203619e03f5382a
OpportunV <RsTGear@gmail.com>
2020-07-22 14:47:56 +0300

    v1.0.0

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import json
-import os
 from typing import Optional, Tuple
 
 import telebot as tb
@@ -9,6 +8,13 @@ import tools
 
 
 def init_bot(token: str, saver: tools.Saver, web_data: Optional[Tuple[str, Flask]] = None) -> tb.TeleBot:
+    """
+    
+    :param token: str, bot token
+    :param saver: tools.Saver instance or other, must have method add
+    :param web_data: optional tuple(url, app), required for webhook
+    :return: telebot.TeleBot instance
+    """
     bot = tb.TeleBot(token, threaded=False)
     
     bot.remove_webhook()
@@ -26,19 +32,28 @@ def init_bot(token: str, saver: tools.Saver, web_data: Optional[Tuple[str, Flask
     def start_handler(message):
         bot.send_message(message.chat.id, "Now you can send audios or photos!\nI'll do something with it.")
         
-    @bot.message_handler(content_types=['audio', 'voice'])
+    @bot.message_handler(content_types=['audio'])
     def audio_handler(message):
         audio = bot.download_file(bot.get_file(message.audio.file_id).file_path)
         
         uid = str(message.chat.id)
-        filename = f"{bot.get_file(message.audio.file_id).file_path.split('/')[-1].split('.')[0]}.wav"
+        filename = bot.get_file(message.audio.file_id).file_path.split('/')[-1]
         
+        saver.add(uid, 'audio', filename, audio)
+
+    @bot.message_handler(content_types=['voice'])
+    def voice_handler(message):
+        audio = bot.download_file(bot.get_file(message.voice.file_id).file_path)
+    
+        uid = str(message.chat.id)
+        filename = bot.get_file(message.voice.file_id).file_path.split('/')[-1]
+    
         saver.add(uid, 'audio', filename, audio)
 
     @bot.message_handler(content_types=['photo'])
     def image_handler(message):
         image = bot.download_file(bot.get_file(message.photo[-1].file_id).file_path)
-        if tools.is_face(image):
+        if tools.contains_face(image):
             uid = str(message.chat.id)
             filename = bot.get_file(message.photo[-1].file_id).file_path.split('/')[-1]
             
--- tools.py
+++ tools.py
@@ -1,16 +1,14 @@
 import io
 import json
 import os
-import wave
-import audioop
 from collections import defaultdict
+from threading import Timer
 
 import face_recognition
-import numpy as np
 
 
 class Saver:
-    def __init__(self):
+    def __init__(self, interval=3600):
         try:
             with open(os.path.join('data', 'data.json')) as f:
                 self.data = defaultdict(dict,json.load(f))
@@ -18,43 +16,65 @@ class Saver:
             self.data = defaultdict(dict)
         except FileNotFoundError:
             self.data = defaultdict(dict)
-    
-    def add(self, uid, type_, filename, file_data):
-        print(self.data)
         
+        # Calling save function every "interval" seconds
+        Timer(interval, self.save).start()
+    
+    def add(self, uid: str, type_: str, filename: str, file_data: bytes):
+        """
+        Adds data to dict object and saves file
+        :param uid: string, user directory
+        :param type_: string, either 'audio' or 'video' , subdirectory for user directory
+        :param filename: string
+        :param file_data: bytes
+        :return: None
+        """
         path = os.path.join(os.path.join('data', uid), type_)
+
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+        with open(os.path.join(path, filename), 'wb') as f:
+            f.write(file_data)
+        
+        if type_ == 'audio':
+            convert_audio(path, filename)
+            filename = f'{filename.rsplit(".", 1)[0]}.wav'
         
         try:
             self.data[uid][type_].append(filename)
         except KeyError:
             self.data[uid][type_] = [filename]
         
-        print(self.data)
-        
-        if not os.path.exists(path):
-            os.makedirs(path)
-            
-        with open(os.path.join(path, filename), 'wb') as f:
-            f.write(file_data)
-            
     def save(self):
+        """
+        saves dict object to json file
+        :return: None
+        """
         with open(os.path.join('data', 'data.json'), 'w') as f:
             json.dump(self.data, f, indent=2)
 
 
-def adjust_audio(audio):
-    return audio
+def convert_audio(path: str, filename:str, rate: int = 16000):
+    """
+    Converts audio executing console ffmpeg command
+    :param path: string
+    :param filename: string
+    :param rate: int, output rate, default to 16000
+    :return: None
+    """
+    full_path = os.path.join(path, filename)
+    full_path_out = f'{full_path.rsplit(".", 1)[0]}.wav'
+    if not os.path.exists(full_path_out):
+        os.system(f'ffmpeg -loglevel quiet -i {full_path} -ar {rate} {full_path_out}')
+    os.remove(full_path)
 
 
-def is_face(image):
+def contains_face(image: bytes) -> bool:
+    """
+    Checks if there is at least one face on the image or not
+    :param image: bytes
+    :return: bool
+    """
     faces = face_recognition.face_locations(face_recognition.load_image_file(io.BytesIO(image)))
     return bool(faces)
-
-
-def main():
-    print(is_face(open('test_data/test_img/games-the-witcher-3-wild-hunt-90309.jpg', 'rb').read()))
-    pass
-
-
-if __name__ == '__main__':
-    main()
